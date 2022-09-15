b5918583dfdf546ca27c789af2148dca89a4dd6f
TirthWohlig <tirthwohlig@gmail.com>
2022-09-15 11:30:25 +0530

    #hi

--- script.js
+++ script.js
@@ -2,26 +2,22 @@ const app = Vue.createApp({
   template: "",
   data() {
     return {
-      // firstName: "John",
-      // lastName: "Doe",
-      // age: 30,
-      // email: "john@gmail.com",
-      // gender: "male",
-      // phone: "123-456-7890",
-      // picture: "	https://randomuser.me/api/portraits/men/10.jpg",
       finalResult: {
-        title: "Mr",
-        firstName: "John",
-        lastName: "Doe",
-        age: 30,
-        email: "john@gmail.com",
-        gender: "male",
-        phone: "123-456-7890",
-        picture: "	https://randomuser.me/api/portraits/men/10.jpg"
-      }
-
-    };
+        gender: {},
+        picture: {
+        },
+        name: {
+        },
+        dob: {
+        }
+      },
+    }
   },
+
+  async created() {
+    this.getUser ()
+  },
+
   methods: {
     async getUser() {
       const res = await fetch("https://randomuser.me/api");
@@ -30,16 +26,10 @@ const app = Vue.createApp({
       console.log(results);
 
       this.finalResult = results[0];
+    }
+  }
 
-      // this.firstName = results[0].name.first;
-      // this.lastName = results[0].name.last;
-      // this.age = results[0].dob.age;
-      // this.email = results[0].email;
-      // this.gender = results[0].gender;
-      // this.picture = results[0].picture.large;
-      // this.phone = results[0].phone;
-    },
-  },
-});
+}
+);
 
 app.mount("#app");
